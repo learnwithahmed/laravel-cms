8fde9913b069116b2d0d217b9c0a9a3fa06708be
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-09-19 22:56:15 +0200

    :lipstick: Add Dropdown for login/logout

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,31 +1,32 @@
 <nav class="navbar is-white">
-    <div class="container">
-        <div class="navbar-brand">
-            <a class="navbar-item brand-text" href="../">
+  <div class="container">
+    <div class="navbar-brand">
+      <a class="navbar-item brand-text" href="../">
                 Employee Management Dashboard
             </a>
-            <div class="navbar-burger burger" data-target="navMenu">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+      <div class="navbar-burger burger" data-target="navMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <div id="navMenu" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="admin.html"> Home</a>
+        <a class="navbar-item" href="admin.html"> Orders </a>
+        <a class="navbar-item" href="admin.html"> Payments </a>
+        <a class="navbar-item" href="admin.html"> Exceptions </a>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item has-dropdown is-hoverable">
+          Profile
+          <div class="navbar-dropdown is-right">
+            <a class="navbar-item"> Overview </a>
+            <a class="navbar-item"> Elements </a>
+            <a class="navbar-item"> Components </a>
+          </div>
         </div>
-        <div id="navMenu" class="navbar-menu">
-            <div class="navbar-start">
-                <a class="navbar-item" href="admin.html">
-                    Home
-                </a>
-                <a class="navbar-item" href="admin.html">
-                    Orders
-                </a>
-                <a class="navbar-item" href="admin.html">
-                    Payments
-                </a>
-                <a class="navbar-item" href="admin.html">
-                    Exceptions
-                </a>
-            </div>
-        </div>
+      </div>
     </div>
 </nav>
 <!-- END NAV -->
