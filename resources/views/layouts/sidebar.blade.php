bde3c154c68b9dcd941da2dc5c0054e5796ea0e2
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-09-26 23:26:38 +0200

    :lipstick: Adding class .is-active to current link in Sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,33 +1,32 @@
 <aside class="menu">
-    <p class="menu-label">
-        General
-    </p>
-    <ul class="menu-list">
-        <li><a class="is-active">Dashboard</a></li>
-        <li><a>Customers</a></li>
-    </ul>
-    <p class="menu-label">
-        Administration
-    </p>
-    <ul class="menu-list">
-        <li><a>Team Settings</a></li>
-        <li>
-            <a>Manage Your Employees</a>
-            <ul>
-                <li><a>Employees</a></li>
-                <li><a>Add a employee</a></li>
-            </ul>
-        </li>
-        <li><a>Invitations</a></li>
-        <li><a>Cloud Storage Environment Settings</a></li>
-        <li><a>Authentication</a></li>
-    </ul>
-    <p class="menu-label">
-        Transactions
-    </p>
-    <ul class="menu-list">
-        <li><a>Payments</a></li>
-        <li><a>Transfers</a></li>
-        <li><a>Balance</a></li>
-    </ul>
+  <p class="menu-label">
+    General
+  </p>
+  <ul class="menu-list">
+    <li><a href={{ route('dashboard') }} class="{{ Request::is('dashboard') ? 'is-active' : '' }}">Dashboard</a></li>
+    <li><a href={{ route('employees.index') }} class="{{ Request::is('employees*') ? 'is-active' : '' }}">Employees</a></li>
+  </ul>
+  <p class="menu-label">
+    Administration
+  </p>
+  <ul class="menu-list">
+    <li><a>Team Settings</a></li>
+    <li>
+      <a>Manage Your Employees</a>
+      <ul>
+        <li><a>Employees</a></li>
+        <li><a>Add a employee</a></li>
+      </ul>
+    </li>
+    <li><a>Invitations</a></li>
+    <li><a>Authentication</a></li>
+  </ul>
+  <p class="menu-label">
+    Transactions
+  </p>
+  <ul class="menu-list">
+    <li><a>Payments</a></li>
+    <li><a>Transfers</a></li>
+    <li><a>Balance</a></li>
+  </ul>
 </aside>
