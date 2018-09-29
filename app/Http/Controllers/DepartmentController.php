070e1b7b1952e3426287876603ecb35e7f825915
Naeleno <naeleno@gmail.com>
2018-09-30 01:08:04 +0300

    implement  Department | migration | model | controller

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Department;
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $employees = Department::all();
+        return view('employees.index')->with('employees', $employees);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('department.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        request()->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+
+
+        Department::create($request->all());
+
+
+        return redirect()->route('department.index')
+            ->with('success','Department created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Department $department)
+    {
+        return view('department.show',compact('department'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Department $department)
+    {
+        return view('department.edit',compact('department'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+
+    //update department
+    public function update(Request $request, Department $department)
+    {
+        request()->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+
+
+        $department->update($request->all());
+
+
+        return redirect()->route('department.index')
+            ->with('success','department updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Department  $department
+     * @return \Illuminate\Http\Response
+     */
+    // delete department
+    public function destroy(Department $department)
+    {
+        $department->delete();
+
+
+        return redirect()->route('department.index')
+            ->with('success','department deleted successfully');
+    }
+}
