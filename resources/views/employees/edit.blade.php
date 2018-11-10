6b5614793c56a5bb048218ede8a4df74950503c2
yahya mohammed <yahayikaby@gmail.com>
2018-11-10 19:34:44 +0300

    create new fields for edit and update

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -4,7 +4,7 @@
 <form method="POST" action="{{ route('employees.update', [$employee->id]) }}">
   @csrf
   {{method_field('PATCH')}}
-  @include('employees.fields')
+  @include('employees.fields_edit')
   <button class="button is-block is-info is-medium is-fullwidth">{{ __('Update') }}</button>
 </form>
 @endsection
