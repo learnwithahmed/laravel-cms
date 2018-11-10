d8eca956489db88dbc32af03ef5b87a40353d0e1
yahya mohammed <yahayikaby@gmail.com>
2018-11-10 19:33:47 +0300

    change rull for update

--- app/Http/Requests/StoreEmployee.php
+++ app/Http/Requests/StoreEmployee.php
@@ -5,30 +5,30 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreEmployee extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            'name'   => 'required',
-            'title'  => 'required',
-            'gender' => 'required',
-            'phone'  => 'required',
-            'email'  => 'required|unique:employees',
-            'city'   => 'required'
-        ];
-    }
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'name' => 'required',
+			'title' => 'required',
+			'gender' => 'required',
+			'phone' => 'required',
+			'email' => 'required|unique:employees,email,' . $this->employee . '|max:200',
+			'city' => 'required',
+		];
+	}
 }
