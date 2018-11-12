6f19b691d52b2d95f7a172a4d0c5f5753c294ae3
yahya mohammed <yahayikaby@gmail.com>
2018-11-12 23:21:56 +0300

    add form and message for search

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+<form action="/search" method="POST" role="search">
+    {{ csrf_field() }}
+    <div class="input-group">
+        <input type="text" class="form-control" name="search" placeholder="Search users">
+           <button type="submit">search</button>
+    </div>
+</form>
+
+@if (Session::has('message'))
+    <div class="alert alert-info">{{ Session::get('message') }}</div>
+@endif
+
     <div class="table-container">
       @include('employees.table')
-    </div>
+	</div>
 @endsection
