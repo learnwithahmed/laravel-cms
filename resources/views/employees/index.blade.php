17a6617290401652e5f43eeca452ba6a0e14e782
yahya mohammed <yahayikaby@gmail.com>
2018-11-13 19:37:17 +0300

    create link for sorting function

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+<div>
+	<a href="?gender=male">Male</a>|
+	<a href="?gender=female">Female</a>|
+	<a href="?descent">Descent</a>|
+	<a href="?ascent">Ascent</a>
+</div>
 <form action="/search" method="POST" role="search">
     {{ csrf_field() }}
     <div class="input-group">
@@ -15,5 +21,6 @@
 
     <div class="table-container">
       @include('employees.table')
-	</div>
+
+
 @endsection
