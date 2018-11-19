87588330976a617b66693d43a8738d5935ea3dd2
yahya mohammed <yahayikaby@gmail.com>
2018-11-19 23:20:28 +0300

    add search function and route

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
-
 @section('content')
-    <div class="table-container">
-      @include('employees.table')
-    </div>
+<form action="/search" method="POST" role="search">
+	{{ csrf_field() }}
+	<div class="input-group">
+		<input type="text" class="form-control" name="search" placeholder="Search users">
+		<button type="submit">search</button>
+	</div>
+</form>
+
+@if (Session::has('message'))
+<div class="alert alert-info">{{ Session::get('message') }}</div>
+@endif
+<div class="table-container">
+	@include('employees.table')
+</div>
 @endsection
