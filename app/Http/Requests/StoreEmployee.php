9c448e775a037b05d077bd6b5628b480be2725bf
yahya mohammed <yahayikaby@gmail.com>
2018-11-23 21:15:19 +0300

    change validation rules for update

--- app/Http/Requests/StoreEmployee.php
+++ app/Http/Requests/StoreEmployee.php
@@ -5,30 +5,35 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreEmployee extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            'name'   => 'required',
-            'title'  => 'required',
-            'gender' => 'required',
-            'phone'  => 'required',
-            'email'  => 'required|unique:employees',
-            'city'   => 'required'
-        ];
-    }
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		$rules = [
+			'name' => 'required',
+			'title' => 'required',
+			'gender' => 'required',
+			'phone' => 'required',
+			'email' => 'required|unique:employees,email,' . $this->employees,
+			'city' => 'required',
+		];
+
+		if (request()->isMethod('PATCH')) {
+			$rules['email'] = 'required';
+		}
+		return $rules;
+	}
 }
